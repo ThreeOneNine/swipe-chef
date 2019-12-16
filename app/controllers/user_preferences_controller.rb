c67a813ee3d68afa79c1fd175bf932a936c629d1
Rob Pickard <rob.pickard11@gmail.com>
2019-12-16 20:58:34 +0000

    Create method added

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -1,19 +1,26 @@
 class UserPreferencesController < ApplicationController
-  before_action :set_user_preference, only: [:index, :update]
-
   def index
+    if UserPreference.where(user: current_user).any?
+      @user_preference = UserPreference.where(user: current_user).first
+    else
+      @user_preference = UserPreference.new
+    end
   end
 
   def update
+    @user_preference = UserPreference.where(user: current_user).first
     @user_preference.update(strong_params)
     render :index
   end
 
-  private
-
-  def set_user_preference
-    @user_preference = UserPreference.where(user: current_user).first
+  def create
+    @user_preference = UserPreference.new(strong_params)
+    @user_preference.user = current_user
+    @user_preference.save
+    render :index
   end
+
+  private
 
   def strong_params
     params.require(:user_preference).permit(:max_cooking_time, :cook_for_min, :cook_for_max)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :recipes, only: [:index, :show]
-  resources :user_preferences, only: [:index, :update]
+  resources :user_preferences, only: [:index, :update, :create]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
