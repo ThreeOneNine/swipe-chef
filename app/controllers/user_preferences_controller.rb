bf7f5f146a70b10dc26a3584ed3a47d148e8f7dc
Rob Pickard <rob.pickard11@gmail.com>
2019-12-16 20:03:51 +0000

    Update method and route added

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -1,5 +1,20 @@
 class UserPreferencesController < ApplicationController
+  before_action :set_user_preference, only: [:index, :update]
+
   def index
-    @user_preferences = UserPreference.where(user: current_user)
+  end
+
+  def update
+    @user_preference.update(strong_params)
+  end
+
+  private
+
+  def set_user_preference
+    @user_preference = UserPreference.where(user: current_user).first
+  end
+
+  def strong_params
+    params.require(:user_preference).permit(:max_cooking_time, :cook_for_min, :cook_for_max)
   end
 end
