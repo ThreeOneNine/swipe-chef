70f733ce0ec87de1af4c1abd6844b752f25843be
Joe Olley <joeolley@hotmail.co.uk>
2019-12-18 15:23:12 +0000

    step count routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :recipes, only: [:index, :show]
+  get 'recipes/:id/next_step/:step', to: 'recipes#next_step'
+  get 'recipes/:id/previous_step/:step', to: 'recipes#previous_step'
   resources :user_preferences, only: [:index, :update, :create]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
