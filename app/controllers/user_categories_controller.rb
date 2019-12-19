8a7987b03f12f1662a0773237ec682a0e0e85a72
Rob Pickard <rob.pickard11@gmail.com>
2019-12-19 16:13:39 +0000

    Index route, method, view added

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -1,2 +1,5 @@
 class UserCategoriesController < ApplicationController
+  def index
+    @user_categories = UserCategories.where(user: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   devise_for :users
 
-  resources :recipes, only: [:index, :show]
   get 'recipes/:id/next_step/:step', to: 'recipes#next_step'
   get 'recipes/:id/previous_step/:step', to: 'recipes#previous_step'
+
+  resources :recipes, only: [:index, :show]
   resources :user_preferences, only: [:index, :update, :create]
-  root to: 'pages#home'
+  resources :user_categories, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
