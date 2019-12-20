c703eb3e51dfd5ec3ff4e37d7bcea92e67c18407
Oliver Inglis <ollie@theinglistwins.com>
2019-12-20 12:48:25 +0000

    first attempt to implement swiper'

---
+++ app/javascript/packs/swiper.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper';
+
+var swiperTest = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+export { swiperTest };
