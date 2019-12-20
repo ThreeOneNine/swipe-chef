6149c2f514b4918810c2aa5da109d20b4f94f1f4
rob pickard <rob.pickard11@gmail.com>
2019-12-20 18:28:39 +0000

    New structure added

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -1,17 +1,16 @@
 class UserCategoriesController < ApplicationController
   def index
-    @user_category = UserCategory.new
-    @user_categories = current_user.user_categories
-    @remaining_categories = Category.all - @user_categories.map(&:category)
+    @categories = Category.all
+    @user_categories = current_user.categories
+    # @remaining_categories = Category.all - @user_categories.map(&:category)
   end
 
   def destroy
     @user_category = UserCategory.find(params[:id])
     @user_category.destroy
 
-    @user_category = UserCategory.new
-    @user_categories = current_user.user_categories
-    @remaining_categories = Category.all - @user_categories.map(&:category)
+    @categories = Category.all
+    @user_categories = current_user.categories
     index_ajax
   end
 
@@ -21,9 +20,8 @@ class UserCategoriesController < ApplicationController
     @user_category.user = current_user
     @user_category.save
 
-    @user_category = UserCategory.new
-    @user_categories = current_user.user_categories
-    @remaining_categories = Category.all - @user_categories.map(&:category)
+    @categories = Category.all
+    @user_categories = current_user.categories
     index_ajax
   end
 
