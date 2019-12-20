165b176c2ae4ae294bcc3ecd578a73512b372aae
rob pickard <rob.pickard11@gmail.com>
2019-12-20 19:05:56 +0000

    Controller dryed

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -1,18 +1,15 @@
 class UserCategoriesController < ApplicationController
+  before_action :set_categories, only: %i[index]
+  after_action :set_categories, only: %i[create destroy]
+  after_action :index_ajax, only: %i[create destroy]
+
   def index
-    @categories = Category.all
-    @user_categories = current_user.categories
-    # @remaining_categories = Category.all - @user_categories.map(&:category)
   end
 
   def destroy
     @user_category = UserCategory.find(params[:id])
     @category = @user_category.category
     @user_category.destroy
-
-    @categories = Category.all
-    @user_categories = current_user.categories
-    index_ajax
   end
 
   def create
@@ -20,13 +17,14 @@ class UserCategoriesController < ApplicationController
     @user_category.category = Category.find(params[:category_id])
     @user_category.user = current_user
     @user_category.save
-
-    @categories = Category.all
-    @user_categories = current_user.categories
-    index_ajax
   end
 
   private
+
+  def set_categories
+    @categories = Category.all
+    @user_categories = current_user.categories
+  end
 
   def index_ajax
     respond_to do |f|
