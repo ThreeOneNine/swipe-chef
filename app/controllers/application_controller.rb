b7453e91d8cb39b37bfa229f506c91bca9af7420
rob pickard <rob.pickard11@gmail.com>
2020-01-01 18:01:23 +0000

    Sign in redirect changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    if UserPreference.where(user: current_user).any?
+    if current_user.user_preferences.any?
       recipes_path
     else
       user_preferences_path
