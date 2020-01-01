810dee6eafd965cceed35738f4f434ebec5f4699
rob pickard <rob.pickard11@gmail.com>
2020-01-01 18:58:53 +0000

    Create method ajaxified

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -10,23 +10,26 @@ class UserPreferencesController < ApplicationController
   def update
     @user_preference = current_user.user_preferences.first
     @user_preference.update!(strong_params)
-
-    respond_to do |f|
-      f.html { redirect_to user_preferences_path }
-      f.js
-    end
+    index_ajax
   end
 
   def create
     @user_preference = UserPreference.new(strong_params)
     @user_preference.user = current_user
     @user_preference.save
-    render :index
+    index_ajax
   end
 
   private
 
   def strong_params
     params.require(:user_preference).permit(:max_cooking_time, :cook_for_min, :cook_for_max)
+  end
+
+  def index_ajax
+    respond_to do |f|
+      f.html { redirect_to user_preferences_path }
+      f.js
+    end
   end
 end
