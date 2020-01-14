c13996063ec899271028fcb1602cb3ab3d9c1574
rob pickard <rob.pickard11@gmail.com>
2020-01-14 12:38:29 +0000

    First system test added for user_preferences

--- test/system/user_preferences_test.rb
+++ test/system/user_preferences_test.rb
@@ -2,23 +2,18 @@ require "application_system_test_case"
 
 class UserPreferencesTest < ApplicationSystemTestCase
   test "Lets a signed in user create a new user preference" do
-   login_as users(:user_1)
-   visit user_preferences_path
-   save_and_open_screenshot
+    login_as users(:user_1)
+    visit user_preferences_path
 
-   fill_in "Maximum cooking time", with: 50
-   fill_in "Minimum servings", with: 2
-   fill_in "Maximum servings", with: 5
-   save_and_open_screenshot
+    fill_in "Maximum cooking time", with: 50
+    fill_in "Minimum servings", with: 2
+    fill_in "Maximum servings", with: 2
+    click_on 'Set Preference'
 
-   click_on 'Set Preference'
-   save_and_open_screenshot
-
-   # Check recipe matches new preferences
-   visit recipes_path
-
-
- end
+    # Check recipe displayed at index matches new preference
+    visit recipes_path
+    assert_selector "#recipe_serves", text: "2 people"
+  end
 end
 
 # # Should be redirected to recipes index
